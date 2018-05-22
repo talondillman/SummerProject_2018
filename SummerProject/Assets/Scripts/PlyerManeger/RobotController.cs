5ae61b47f26d0a6722812434d98f8e0da7f30e1f
Talon <cptHammerFall@users.noreply.github.com>
2018-05-22 15:08:49 -0600

    Just a 3D working scene change.

--- SummerProject/Assets/Scripts/PlyerManeger/RobotController.cs
+++ SummerProject/Assets/Scripts/PlyerManeger/RobotController.cs
@@ -12,7 +12,7 @@ public class RobotController : MonoBehaviour
     private bool grounded = false;
     private bool facingRight = true;
     private float speed = 0;
-    private Rigidbody2D player;
+    private Rigidbody player;
     
     //Used to see if on the ground
     public Transform groundCheck;
@@ -24,51 +24,56 @@ public class RobotController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        player = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        player = GetComponent<Rigidbody>();
+        //anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //Checks all variables to see if on ground
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-        anim.SetFloat("vSpeed", player.velocity.y);
-        anim.SetBool("Ground", grounded);
+        //grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        //anim.SetFloat("vSpeed", player.velocity.y);
+        //anim.SetBool("Ground", grounded);
 
         float move = Input.GetAxis("Horizontal");
 
-        anim.SetFloat("Speed", Mathf.Abs(move));
+        //anim.SetFloat("Speed", Mathf.Abs(move));
 
         player.velocity = new Vector2(move * maxSpeed, player.velocity.y);
 
         //Flips player if moving other direction
-        if (move > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (move < 0 && facingRight)
-        {
-            Flip();
-        }
+        //if (move > 0 && !facingRight)
+        //{
+        //    Flip();
+        //}
+        //else if (move < 0 && facingRight)
+        //{
+        //    Flip();
+        //}
     }
     private void Update()
     {
-        //Only allow jump while on ground
-        if (grounded && Input.GetKeyDown(KeyCode.Space))//Hardcoded to space bar
-        {
-            anim.SetBool("Ground", false);
-            player.AddForce(new Vector2(0, jumpforce));
-        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        //var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        //transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, 0);
+        ////Only allow jump while on ground
+        //if (grounded && Input.GetKeyDown(KeyCode.Space))//Hardcoded to space bar
+        //{
+        //    anim.SetBool("Ground", false);
+        //    player.AddForce(new Vector2(0, jumpforce));
+        //}
     }
     //Makes sprite face the correct direction
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+    //void Flip()
+    //{
+    //    facingRight = !facingRight;
+    //    Vector3 theScale = transform.localScale;
+    //    theScale.x *= -1;
+    //    transform.localScale = theScale;
+    //}
 
     //used to change UI elements
     private void OnGUI()
