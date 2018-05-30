a6a6c83d81ec6c835d3ac47d3a7f530dc371877b
angeliqueaban <angeliqueaban@gmail.com>
2018-05-29 20:38:28 -0600

    Working on battle menu

--- SummerProject/Assets/Scripts/BattleMenu/BattleMenu.cs
+++ SummerProject/Assets/Scripts/BattleMenu/BattleMenu.cs
@@ -4,20 +4,64 @@ using UnityEngine;
 
 public class BattleMenu : MonoBehaviour {
     public bool basicAtk;
-	// Use this for initialization
-	void Start () {
+    private BattleTurnStart battleStart = new BattleTurnStart();
+    // Use this for initialization
+    public enum BattleTurns
+    {
+        START,
+        P1,
+        P2,
+        ENEMY,
+        LOSE,
+        WIN,
+        CALCDAMAGE
+    }
+
+    private BattleTurns currentState;
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-    private void OnMouseUpAsButton()
-    {
-        if (basicAtk)
+        Debug.Log(currentState);
+        switch (currentState)
         {
-            GUI.Button(new Rect(Screen.width / 2, 150, 100, 30), "Attack 1");
+            case (BattleTurns.START):
+                //SETUP BATTLE
+                battleStart.prepBattle();
+                break;
+            case (BattleTurns.P1):
+                //SETUP BATTLE
+                break;
+            case (BattleTurns.P2):
+                //SETUP BATTLE
+                break;
+            case (BattleTurns.ENEMY):
+                //SETUP BATTLE
+                break;
+            case (BattleTurns.CALCDAMAGE):
+                //SETUP BATTLE
+                break;
+            case (BattleTurns.LOSE):
+                //SETUP BATTLE
+                break;
+            case (BattleTurns.WIN):
+                //SETUP BATTLE
+                break;
+
+
+        }
+	}
+ 
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Next State"))
+        {
+            if (currentState == BattleTurns.START)
+            {
+                currentState = BattleTurns.P1;
+            }
         }
     }
 }
