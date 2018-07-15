faee209cc11645889b4f33ee98292ec4f24a0ae0
Jarom Norris <jarom.norris@gmail.com>
2018-07-15 11:58:31 -0600

    Stage Materials Underway!

---
+++ SummerProject/Assets/Scripts/DanceScreenSequence.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceScreenSequence : MonoBehaviour {
+
+    public Texture[] screens;
+    MeshRenderer mRender;
+
+	// Use this for initialization
+	void Start () {
+        mRender = GetComponent<MeshRenderer>();
+
+        Invoke("ChangeScreen", 0.3871f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ChangeScreen()
+    {
+        Material[] mats;
+        mats = mRender.materials;
+        mats[3].SetTexture("_MainTex", screens[Random.Range(0, screens.Length)]);
+        mRender.materials = mats;
+
+        Invoke("ChangeScreen", 0.3871f);
+    }
+}
---
+++ SummerProject/Assets/Scripts/LightSequence.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSequence : MonoBehaviour {
+
+    public int SequenceNumber = 0;
+    private int CurrentNumber = 0;
+    
+    // Use this for initialization
+	void Start () {
+        if (SequenceNumber != 0)
+            gameObject.SetActive(false);
+
+        Invoke("ProgressSequence", 0.3871f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ProgressSequence()
+    {
+        CurrentNumber++;
+        if (CurrentNumber == 4)
+            CurrentNumber = 0;
+
+        if (SequenceNumber != CurrentNumber)
+            gameObject.SetActive(false);
+        else
+            gameObject.SetActive(true);
+
+        Invoke("ProgressSequence", 0.3871f);
+    }
+}
